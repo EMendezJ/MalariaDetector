f6285db9ad85f77adecec2dcbd15307b39176707
vero0996 <wab5119@gmail.com>
2025-04-09 10:40:16 -0600

    Update tareagraficacion.m

--- tareagraficacion.m
+++ tareagraficacion.m
@@ -1,5 +1,5 @@
 % Script para visualizar el campo eléctrico de un dipolo
-
+% Integrantes del equipo: Emiliano Mendez, Oscar Ortiz, Luis Salazar, Veronica Zapata
 % Parámetros constantes
 q1 = 1e-6;          % Carga positiva (C)
 q2 = -1e-6;         % Carga negativa (C)
@@ -11,12 +11,14 @@ N = 20;             % Resolución de la malla
 lim = 1;            % Límites del espacio
 kC = 9e9;           % Constante de Coulomb (N·m^2/C^2)
 
-% Crear malla
+% Crear malla usando la funcion de meshgrid
 [xG, yG] = meshgrid(linspace(-lim, lim, N), linspace(-lim, lim, N));
+
+% Iniciación de la matriz de los ampos electricos en el eje x y en el eje y
 Ex = zeros(size(xG));
 Ey = zeros(size(yG));
 
-% Figura para graficar
+% Figura para graficar 
 figure('Name', 'Campo Eléctrico de Dipolos');
 subplot(1, 2, 1); % Subfigura para el campo vectorial
 
@@ -24,18 +26,24 @@ subplot(1, 2, 1); % Subfigura para el campo vectorial
 for i = 1:size(xG, 1)
     for j = 1:size(xG, 2)
         % Campo por q1
+        % Se cacula el vector que va desde un punto hasta la carga
         r1x = xG(i, j) - pos1(1);
         r1y = yG(i, j) - pos1(2);
         r1 = sqrt(r1x^2 + r1y^2);
-        % Usar la funcion matemática eps para evitar disminuir los errores
+        
+        % Usar la funcion matemática eps para disminuir los errores
         % por redondeo
         Ex1 = kC * q1 * r1x / (r1^3 + eps);
         Ey1 = kC * q1 * r1y / (r1^3 + eps);
 
         % Campo por q2
+        % Se cacula el vector que va desde un punto hasta la carga
         r2x = xG(i, j) - pos2(1);
         r2y = yG(i, j) - pos2(2);
         r2 = sqrt(r2x^2 + r2y^2);
+
+        % Usar la funcion matemática eps para disminuir los errores
+        % por redondeo
         Ex2 = kC * q2 * r2x / (r2^3 + eps);
         Ey2 = kC * q2 * r2y / (r2^3 + eps);
 
@@ -48,7 +56,7 @@ end
 % Magnitud del campo eléctrico
 E = sqrt(Ex.^2 + Ey.^2);
 
-% Normalización para graficar vectores
+% Calcular el vector unitario de x y de y para graficar vectores
 u = Ex ./ (E + eps); 
 v = Ey ./ (E + eps);
 
@@ -78,13 +86,16 @@ for i = 1:length(x_points)
     r1x = x_points(i) - pos1(1);
     r1y = 0.5 - pos1(2);
     r1 = sqrt(r1x^2 + r1y^2);
+    
     r2x = x_points(i) - pos2(1);
     r2y = 0.5 - pos2(2);
     r2 = sqrt(r2x^2 + r2y^2);
+    
     Ex1 = kC * q1 * r1x / (r1^3 + eps);
     Ey1 = kC * q1 * r1y / (r1^3 + eps);
     Ex2 = kC * q2 * r2x / (r2^3 + eps);
     Ey2 = kC * q2 * r2y / (r2^3 + eps);
+    
     Ex_total = Ex1 + Ex2;
     Ey_total = Ey1 + Ey2;
     E_y05(i) = sqrt(Ex_total^2 + Ey_total^2);
@@ -92,12 +103,15 @@ for i = 1:length(x_points)
     % y = 1
     r1y = 1 - pos1(2);
     r1 = sqrt(r1x^2 + r1y^2);
+    
     r2y = 1 - pos2(2);
     r2 = sqrt(r2x^2 + r2y^2);
+    
     Ex1 = kC * q1 * r1x / (r1^3 + eps);
     Ey1 = kC * q1 * r1y / (r1^3 + eps);
     Ex2 = kC * q2 * r2x / (r2^3 + eps);
     Ey2 = kC * q2 * r2y / (r2^3 + eps);
+    
     Ex_total = Ex1 + Ex2;
     Ey_total = Ey1 + Ey2;
     E_y1(i) = sqrt(Ex_total^2 + Ey_total^2);
